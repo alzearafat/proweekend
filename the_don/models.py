d6055f16b4e5cd813d5263c8f09bd1cf34f16f0a
Alzea Arafat <alzea.arafat@live.com>
2015-10-27 00:27:11 +0700

    Add model's help_text

--- the_don/models.py
+++ the_don/models.py
@@ -3,13 +3,13 @@ from ckeditor.fields import RichTextField
 
 class Team(models.Model):
 	name = models.CharField(max_length=200)
-	card_image_header = models.ImageField(upload_to='images/%Y/%m/%d', blank=True)
+	card_image_header = models.ImageField(upload_to='images/%Y/%m/%d', help_text='Ini gambar cover card profile nya. Kecil aja gambar nya.', blank=True)
 	profile_pic = models.ImageField(upload_to='images/%Y/%m/%d', blank=True)
-	quote = models.CharField(max_length=255, blank=True)
-	facebook = models.URLField(max_length=255, blank=True)
-	instagram = models.URLField(max_length=255, blank=True)
-	twitter = models.URLField(max_length=255, blank=True)
-	googleplus = models.URLField(max_length=255, blank=True)
+	quote = models.CharField(max_length=255, help_text='Singkat aja!', blank=True)
+	facebook = models.URLField(max_length=255, help_text='(Opsional)', blank=True)
+	instagram = models.URLField(max_length=255, help_text='(Opsional)', blank=True)
+	twitter = models.URLField(max_length=255, help_text='(Opsional)', blank=True)
+	googleplus = models.URLField(max_length=255, help_text='(Opsional)', blank=True)
 	about = RichTextField(config_name='default')
 	pubdate = models.DateField(auto_now_add=True)
 
--- vblog/models.py
+++ vblog/models.py
@@ -3,18 +3,18 @@ from ckeditor.fields import RichTextField
 
 class Video(models.Model):
 	title = models.CharField(max_length=200)
-	featured_image = models.ImageField(upload_to='images/%Y/%m/%d', blank=True)
-	iframe_src = models.CharField(max_length=255)
-	vimeo_page = models.URLField(max_length=255, blank=True)
-	location = models.CharField(max_length=255, blank=True)
-	HTM = models.CharField(max_length=100, blank=True)
+	featured_image = models.ImageField(upload_to='images/%Y/%m/%d', help_text='<strong>Dimensi gambar maksimal 750x750p, dan minimal 350x350p!</strong>', blank=True)
+	iframe_src = models.CharField(max_length=255, help_text='Cukup copy paste <strong>iframe src</strong> nya saja. Jangan semua embed code nya!')
+	vimeo_page = models.URLField(max_length=255, help_text='URL ke video di vimeo nya (Opsional)', blank=True)
+	location = models.CharField(max_length=255, help_text='Lokasi tempat jalan-jalan nya (Opsional)', blank=True)
+	HTM = models.CharField(max_length=100, help_text='Harga Tiket Masuk, tempat jalan-jalan nya (Opsional)', blank=True)
 	content = RichTextField(config_name='default')
-	gmap_iframe = models.TextField(blank=True)
-	gallery_image_1 = models.ImageField(upload_to='images/%Y/%m/%d', blank=True)
-	gallery_image_2 = models.ImageField(upload_to='images/%Y/%m/%d', blank=True)
-	gallery_image_3 = models.ImageField(upload_to='images/%Y/%m/%d', blank=True)
-	gallery_image_4 = models.ImageField(upload_to='images/%Y/%m/%d', blank=True)
-	pubdate = models.DateField(auto_now_add=True)
+	gmap_iframe = models.TextField(help_text='Copy paste embed code Google Maps nya disini (Opsional)', blank=True)
+	gallery_image_1 = models.ImageField(upload_to='images/%Y/%m/%d', help_text='Dimensi gambar maksimal 750x750p, dan minimal 350x350p! (Opsional)', blank=True)
+	gallery_image_2 = models.ImageField(upload_to='images/%Y/%m/%d', help_text='Dimensi gambar maksimal 750x750p, dan minimal 350x350p! (Opsional)', blank=True)
+	gallery_image_3 = models.ImageField(upload_to='images/%Y/%m/%d', help_text='Dimensi gambar maksimal 750x750p, dan minimal 350x350p! (Opsional)', blank=True)
+	gallery_image_4 = models.ImageField(upload_to='images/%Y/%m/%d', help_text='Dimensi gambar maksimal 750x750p, dan minimal 350x350p! (Opsional)', blank=True)
+	pubdate = models.DateField(help_text='Publication Date', auto_now_add=True)
 
 	def __str__(self):
 		return self.title
