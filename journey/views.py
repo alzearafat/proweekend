2649a1ad13358b0248b62bbb7238a9622df97933
Alzea Arafat <alzea.arafat@live.com>
2015-11-11 13:56:04 +0700

    Add pagination on single journey maker

--- journey/views.py
+++ journey/views.py
@@ -7,20 +7,17 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def journey_list(request):
     post_list = Post.objects.all()
-    paginator = Paginator(post_list, 6) # Show 25 post per page
+    paginator = Paginator(post_list, 6)
 
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
         posts = paginator.page(1)
     except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
         posts = paginator.page(paginator.num_pages)
 
     return render(request, 'journey/journey_list.html', {"posts": posts})
-
 
 def journey_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
@@ -29,6 +26,14 @@ def journey_detail(request, pk):
 def journey_maker(request):
     if request.user.is_authenticated():
         posts = request.user.user_posts.filter()
+        paginator = Paginator(posts, 6)
+        page = request.GET.get('page')
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
         return render(request, 'journey/journey_maker.html', {'posts': posts})
     else:
         return redirect('/accounts/login')
